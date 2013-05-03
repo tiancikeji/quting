6f3a24009e6b662c9fb54fcb7b17777349ce27a1
王雷 <wanglei@chinacache.com>
2013-05-03 13:59:31 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quting::Application.config.secret_token = 'a7969a3cda8e2402063ff5f3e2d5ecd27fdcdf30be5e0102c63b5a1dccc1d42304c969a202ce3fafb965465254c19628638f39bd19e53a3496db9a811da1c609'
