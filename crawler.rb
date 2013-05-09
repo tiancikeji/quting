3a85f76020397c5896cc6b0e53459a08618ee806
王雷 <wanglei@chinacache.com>
2013-05-09 13:32:19 +0800

    update

--- crawler.rb
+++ crawler.rb
@@ -1,17 +1,29 @@
 require 'rubygems'
 require 'nokogiri'
+require 'httparty'
 require 'open-uri'
 
 BASE_URL = "http://www.huaxiazi.com"
-LIST_URL = "#{BASE_URL}"
+INDEX_URL = "#{BASE_URL}"
+Productinfo_URL = "#{BASE_URL}/productinfo.aspx?id="
 
-doc = Nokogiri::HTML(open(LIST_URL))
-i = 0
-doc.xpath('//a[@href]').each do |link|
-  href = link['href']
-  if href.match("Productinfo")
-    p href
-    i = i+1
+# n = 10
+# while n > 0 do
+  doc = Nokogiri::HTML(open(Productinfo_URL+"42863"))
+  doc.xpath('//a[@href]').each do |link|
+    href = link['href']
+    if href.match("Play.aspx")
+      id = href.split("id=")[1].split("&")[0]
+      xml = Nokogiri::XML(open("http://huaxiazi.com/ajax/GetPlayInfo.aspx?PAID="+id))
+      # p xml 
+      xml.xpath("//root/NowPlay").map do |i|
+        p i.xpath("Title").children.text
+        p i.xpath("Length").children.text
+        p i.xpath("Url").children.text
+      end
+    end
   end
-end
-p i
+  
+  # n=n-1
+# end
+
