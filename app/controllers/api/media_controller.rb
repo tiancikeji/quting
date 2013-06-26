0030688c635af563d3923d7b59ba1c9eb0c4a35d
王雷 <wanglei@chinacache.com>
2013-06-26 09:15:05 +0800

    update

--- app/controllers/api/media_controller.rb
+++ app/controllers/api/media_controller.rb
@@ -7,6 +7,16 @@ class Api::MediaController < ApplicationController
     else
       @media = Medium.page params[:page]
     end
+    if params[:guest_id]
+      @likes = Like.where(:guest_id => params[:guest_id])
+      like_ids = []
+      @likes.each do |like|
+        like_ids << like.medium_id
+      end
+      @media.each do |m|
+        m.is_like = 1
+      end
+    end
     render :json => { :count=> Medium.all.count, :media => @media }
   end
 
